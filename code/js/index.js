47d1ec767ac7d16cf66f82f09d146c05dca86ef0
LiangMo <914075513@qq.com>
2017-11-23 22:41:48 +0800

    second commit

--- code/js/index.js
+++ code/js/index.js
@@ -1,34 +1,34 @@
-$(function(){
-    // 1、点击第八个菜单进行切换
-    $("#menu").on("click",".row>div:nth-child(8)",function(){
-        // alert(1)
+// 点击更多进行切换
+$(function () {
+    $("#menu").on("click", ".row>div:nth-child(8)", function () {
         $("#menu>.row>div:nth-last-child(-n+4)").toggle();
+        return false;
     })
-    // 获取菜单样式
     getMenuStyle();
-
     getRecommendStyle();
+
+
 })
 
-// 获取menu的数据
-function getMenuStyle(){
+// 获取menu数据
+function getMenuStyle() {
     $.ajax({
-        url:url+"api/getindexmenu",
-        success:function(data){
-            // console.log(data);
-            var html = template("menuTpl",data);
+        url: url + "api/getindexmenu",
+        success: function (data) {
+            // console.log(data)
+            var html = template("menuTpl", data);
             $("#menu .row").html(html);
         }
     })
 }
 
-function getRecommendStyle(){
+function getRecommendStyle() {
     $.ajax({
-        url:url+"api/getmoneyctrl",
-        success:function(info){
-            // console.log(info);
-            var html = template("getRecommendStyle",info);
+        url: url + "api/getmoneyctrl",
+        success: function (data) {
+            var html = template("recommendTpl", data);
             $("#recommend .recommend_product").html(html);
         }
     })
 }
+
